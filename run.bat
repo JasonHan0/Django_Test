5b923f7a29021527d01dc78635eb077cf4eaae4b
jasonhan <67450735+JasonHan0@users.noreply.github.com>
2022-03-26 13:03:21 +0900

    .

--- run.bat
+++ run.bat
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 python manage.py makemigrations
 python manage.py migrate
-=======
-python manage.py makemigrations
-python manage.py migrate
->>>>>>> e45056403b964f21e99db7b38d412e798b90cf48
 python manage.py runserver
