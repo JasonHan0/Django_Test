77086a7cc5c0f12dc40bde7ed863da4b57741591
jasonhan <67450735+JasonHan0@users.noreply.github.com>
2022-03-27 12:51:24 +0900

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['15.165.77.106']
 
