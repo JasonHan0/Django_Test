e92da2fbda70375897a79aa9fa3ca65f89b192fb
root <root@ip-172-26-2-210.ap-northeast-2.compute.internal>
2022-03-28 03:13:40 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,5 +3,8 @@ from .base import *
 # 보통 운영 환경을 production 환경 (prod.py의 prod는 production의 약어)
 
 ALLOWED_HOSTS = ['15.165.77.106']
-
+STATIC_ROOT = BASE_DIR / 'static/'
+# 관리자 앱의 정적 파일을 STATIC_ROOT 디렉터리로 복사
+STATICFILES_DIRS = []
+# base.py 파일에 STATICFILES_DIRS 항목이 이미 있으니 prod.py 파일에 다시 빈 값으로 설정해서 오류 방지 
 # [배포 시작시 명령어] python manage.py runserver --settings=config.settings.prod
